835d9bf18f0eac200e6a8fcee5f9ae76ac4f89b8
Timur <68633358+TimGon@users.noreply.github.com>
2024-03-30 13:40:08 +0400

    Update lab5.py

--- lab5.py
+++ lab5.py
@@ -22,17 +22,15 @@ def recursive_F(n):
     if n < 2:
         return 1
     if n % 2 == 0:
-        return -1 ** n * (2 * recursive_F(n - 1) + recursive_F(n - 3))
+        return ((-1) ** n) * (2 * recursive_F(n - 1) + recursive_F(n - 3) / factorial_double(n))
     else:
-        return 5 * recursive_F(n - 1) / factorial_double(n) - recursive_F(n - 3)
+        return ((-1) ** n) * (2 * recursive_F(n - 1) + recursive_F(n - 3) / factorial_double(n))
 
 
-while True:
+n = int(input("Введите натуральное число: "))
+while not (0 < n < 50):
+    print("Это число не является натуральным или превышает границу. Повторите ввод.")
     n = int(input("Введите натуральное число: "))
-    if n > 0 and n < 50:
-        break
-    else:
-        print("Это число не является натуральным/превышает границу. Повторите ввод.")
 
 start_time = time.time()
 dp = [0 for i in range(n + 1)]
@@ -41,11 +39,10 @@ dp[1] = 1
 
 for i in range(2, n + 1, 1):
     if i % 2 == 0:
-        dp[i] = (-1) ** i * (2 * dp[i - 1] + dp[i - 3])
-        print(f"dp{i} при четном:", dp[i])
+        dp[i] = (-1) ** i * (2 * dp[i - 1] + dp[i - 3]) / factorial_double(n)
     else:
-        dp[i] = 5 * dp[i - 1] / factorial_double(n) - dp[i - 3]
-        print(f"dp{i} при нечетном:", dp[i])
+        dp[i] = (-1) ** i * (2 * dp[i - 1] + dp[i - 3]) / factorial_double(n)
+
 iterative_time = time.time() - start_time
 result_iterative = dp[n]
 
@@ -56,9 +53,18 @@ recursive_time = time.time() - start_time
 print(f"Рекурсивный результат для F({n}): {result_recursive}, время выполнения: {recursive_time} секунд")
 print(f"Итеративный результат для F({n}): {result_iterative}, время выполнения: {iterative_time} секунд")
 
+table_data = [["Метод", "Значение n", "Время выполнения (секунды)"],
+              ["Рекурсивный", n, recursive_time],
+              ["Итеративный", n, iterative_time]]
+
 plt.figure(figsize=(10, 6))
-plt.plot(n, recursive_time, label='Рекурсивный метод', marker='o')
-plt.plot(n, iterative_time, label='Итеративный метод', marker='x')
+plt.table(cellText=table_data, loc='center', cellLoc='center', colWidths=[0.2, 0.2, 0.2])
+plt.axis('off')
+plt.show()
+
+plt.figure(figsize=(10, 6))
+plt.plot([n], [recursive_time], label='Рекурсивный метод', marker='o')
+plt.plot([n], [iterative_time], label='Итеративный метод', marker='x')
 plt.xlabel('Значение n')
 plt.ylabel('Время выполнения (секунды)')
 plt.title('Сравнение времени выполнения методов')
