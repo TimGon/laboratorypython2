2e41800d7c38051339d02721a57f5a39198a45cc
Timur <68633358+TimGon@users.noreply.github.com>
2024-04-01 17:53:44 +0400

    Update lab5.py

--- lab5.py
+++ lab5.py
@@ -7,67 +7,63 @@
 
 8.	F(n<2)=1;F(n)=(-1)^n*(2F(n-1)+F(n-3))(при n четном),F(n)= 5F(n-1)/(2n)!- F(n-3)(при n нечетном)
 """
-import time
+import cProfile
+import timeit
 import matplotlib.pyplot as plt
-
-
-def factorial_double(k):
-    result = 2
-    for j in range(2, 2*k + 1):
-        result *= j
-    return result
 
 
 def recursive_F(n):
     if n < 2:
         return 1
-    if n % 2 == 0:
-        return ((-1) ** n) * (2 * recursive_F(n - 1) + recursive_F(n - 3) / factorial_double(n))
-    else:
-        return ((-1) ** n) * (2 * recursive_F(n - 1) + recursive_F(n - 3) / factorial_double(n))
+    return (pow(-1, n)) * ((2 * recursive_F(n - 1) + recursive_F(n - 3)) / (2 * factorial(n)))
 
+
+def iterative_F(n):
+    if n < 2:
+        return 1
+    dp = [0 for i in range(n + 1)]
+    dp[0], dp[1] = 1, 1
+    for i in range(2, n + 1, 1):
+        dp[i] = pow(-1, i) * (2 * dp[i - 1] + dp[i - 3]) / factorial(n)
+
+    return dp[n]
+
+
+def factorial(n):
+    if n == 0:
+        return 1
+    result = 1
+    for i in range(2, 2*n + 1, 2):
+        result *= i
+
+    return result
 
 n = int(input("Введите натуральное число: "))
 while not (0 < n < 50):
     print("Это число не является натуральным или превышает границу. Повторите ввод.")
     n = int(input("Введите натуральное число: "))
 
-start_time = time.time()
-dp = [0 for i in range(n + 1)]
-dp[0] = 1
-dp[1] = 1
+results_table = []
 
-for i in range(2, n + 1, 1):
-    if i % 2 == 0:
-        dp[i] = (-1) ** i * (2 * dp[i - 1] + dp[i - 3]) / factorial_double(n)
-    else:
-        dp[i] = (-1) ** i * (2 * dp[i - 1] + dp[i - 3]) / factorial_double(n)
+cProfile.runctx('''
+for n in range(1, n + 1):
+    recursive_time = timeit.timeit('recursive_F(n)', globals=globals(), number=10) * 10
+    iterative_time = timeit.timeit('iterative_F(n)', globals=globals(), number=10) * 10
 
-iterative_time = time.time() - start_time
-result_iterative = dp[n]
+    results_table.append((n, recursive_time, iterative_time))
+''', globals=globals(), locals=locals(), sort='cumulative')
 
-start_time = time.time()
-result_recursive = recursive_F(n)
-recursive_time = time.time() - start_time
+print("+-" + "-"*12 + "-+-" + "-"*12 + "-+-" + "-"*15 + "-+")
+print("| {:^14} | {:^14} | {:^17} |".format("n", "Recursive", "Iterative"))
+print("+-" + "-"*12 + "-+-" + "-"*12 + "-+-" + "-"*15 + "-+")
+for row in results_table:
+    print("| {:^14} | {:^14.6f} | {:^17.6f} |".format(row[0], row[1], row[2]))
+print("+-" + "-"*13 + "-+-" + "-"*12 + "-+-" + "-"*15 + "-+")
 
-print(f"Рекурсивный результат для F({n}): {result_recursive}, время выполнения: {recursive_time} секунд")
-print(f"Итеративный результат для F({n}): {result_iterative}, время выполнения: {iterative_time} секунд")
-
-table_data = [["Метод", "Значение n", "Время выполнения (секунды)"],
-              ["Рекурсивный", n, recursive_time],
-              ["Итеративный", n, iterative_time]]
-
-plt.figure(figsize=(10, 6))
-plt.table(cellText=table_data, loc='center', cellLoc='center', colWidths=[0.2, 0.2, 0.2])
-plt.axis('off')
-plt.show()
-
-plt.figure(figsize=(10, 6))
-plt.plot([n], [recursive_time], label='Рекурсивный метод', marker='o')
-plt.plot([n], [iterative_time], label='Итеративный метод', marker='x')
-plt.xlabel('Значение n')
-plt.ylabel('Время выполнения (секунды)')
-plt.title('Сравнение времени выполнения методов')
+plt.plot([row[0] for row in results_table], [row[1] for row in results_table], label='Рекурсивный метод')
+plt.plot([row[0] for row in results_table], [row[2] for row in results_table], label='Итерационный метод')
+plt.xlabel('n')
+plt.ylabel('Время (сек.)')
+plt.title('Сравнение время между Рекурсией и Итерационным методом')
 plt.legend()
-plt.grid()
 plt.show()
